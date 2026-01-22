70ee3fb315b160b0dc44ea57ed7c61222a4edefa
Bun Panin <169080351+bunpanin@users.noreply.github.com>
2026-01-22 19:59:56 +0700

    Update Credentials Telegram

--- vars/sendTelegramMessageV2.groovy
+++ vars/sendTelegramMessageV2.groovy
@@ -1,18 +1,11 @@
 def call(String message) {
-                
-    withCredentials([usernamePassword(credentialsId: 'TELEGRAM_BOT',
-    passwordVariable: 'TOKEN', usernameVariable: 'CHAT_ID')]) {
-      def encodedMessage = URLEncoder.encode(message, "UTF-8")
+    
+    withCredentials([usernamePassword(credentialsId: 'TELEGRAM_BOT', passwordVariable: 'CHAT_ID', usernameVariable: 'TOKEN')]) {
+
+    def encodedMessage = URLEncoder.encode(message, "UTF-8")
     sh """
         curl -s -X POST https://api.telegram.org/bot${TOKEN}/sendMessage \\
         -d chat_id=${CHAT_ID} \\
         -d text="${encodedMessage}" > /dev/null
     """
-
-           
-        
-        
-    }     
-
-    
 }
